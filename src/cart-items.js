0749ac59eddceab62f92de13a16bcbd10fd115f1
calebbenjin <calebben94@gmail.com>
2021-11-09 13:39:05 +0100

    Starter-Kit Setup

---
+++ src/cart-items.js
@@ -0,0 +1,35 @@
+import Image1 from './img/products/xproduct1.webp'
+import Image2 from './img/products/xproduct2.webp'
+import Image5 from './img/products/xproduct5.webp'
+import Image8 from './img/products/xproduct8.webp'
+
+export default [
+  {
+    id: 1,
+    title: "Quartz Belt Watch",
+    price: 399.99,
+    img: Image1,
+    amount: 1
+  },
+  {
+    id: 2,
+    title: "Women Freshwash",
+    price: 499.99,
+    img: Image2,
+    amount: 1
+  },
+  {
+    id: 3,
+    title: "Man Office Bag",
+    price: 799.99,
+    img: Image5,
+    amount: 1
+  },
+  {
+    id: 4,
+    title: "Charging Car",
+    price: 599.99,
+    img: Image8,
+    amount: 1
+  }
+];
